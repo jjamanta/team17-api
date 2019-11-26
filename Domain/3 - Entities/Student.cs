334a8e18b5363665baf09f773108e534d1e4f47f
Paulo Barros <pbarros@sharenj.org>
2019-11-26 17:41:46 -0300

    Student : Person

--- Domain/3 - Entities/Student.cs
+++ Domain/3 - Entities/Student.cs
@@ -3,8 +3,8 @@ using System;
 
 namespace Team17.Domain.Entities
 {
-	public class Student : _EntityBase
-	{
+	public class Student : Person
+    {
 		public string StudentId { get; set; }
 		public string SchoolId { get; set; }
 		public byte GradeId { get; set; }
